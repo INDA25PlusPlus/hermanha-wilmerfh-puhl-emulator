53d9d9a78ff69fccdd3fe1e60c97ff842731986c
Wilmer Fredriksson Handler <wilmerfhandler@gmail.com>
2025-11-05 12:05:05 +0100

    working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod chip8;
+mod tui;
+
+use std::io;
+use tui::App;
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::new().run(&mut terminal);
+    ratatui::restore();
+    app_result
 }
